d99b47e4e6466912c6d591a53663ac60ed93b1d8
Pikarilles <pikarilles@gmail.com>
2026-01-10 21:06:16 -0700

    Add functionality for merging items

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -22,11 +22,19 @@ var possible_generators = [
 
 var grid = [];
 
+var first_touch = Vector2(0, 0);
+var final_touch = Vector2(0, 0);
+var controlling = false;
+var test;
+
 func _ready() -> void:
 	grid = fill_grid_array();
-	var first_generator = possible_items[0].instantiate();
-	add_child(first_generator);
-	insert_item(first_generator);
+	for i in width:
+		for j in (height/2):
+			var first_generator = possible_items[0].instantiate();
+			add_child(first_generator);
+			insert_item(first_generator);
+			global.move_item.connect(on_move_item);
 
 func fill_grid_array():
 	var array = [];
@@ -37,14 +45,25 @@ func fill_grid_array():
 	return array;
 
 func grid_to_pixel(column, row):
-	var new_x = x_start + offset * column;
-	var new_y = y_start + -offset * row;
+	var new_x = x_start + (offset * column);
+	var new_y = y_start + (offset * row);
 	return Vector2(new_x, new_y);
+
+func pixel_to_grid(pixel_x, pixel_y):
+	var new_x = round((pixel_x - x_start) / offset);
+	var new_y = round((pixel_y - y_start) / offset);
+	return Vector2(new_x, new_y);
+	
+func is_in_grid(column, row):
+	if column >= 0 && column < width:
+		if row >= 0 && row < height:
+			return true;
+	return false;
 
 func find_empty_tile():
 	for i in width:
 		for j in height:
-			if grid[i][j] == []:
+			if not grid[i][j]:
 				return Vector2(i, j);
 	return null;
 
@@ -53,16 +72,67 @@ func insert_item(item: Item):
 	var empty_space = find_empty_tile()
 	# Add item to the empty tile
 	if empty_space != null:
-		print(empty_space);
 		grid[empty_space.x][empty_space.y] = item;
 		item.position = grid_to_pixel(empty_space.x, empty_space.y);
-		print (item.position);
 	else:
 		# If the item is a generator, backlog it for when an empty tile exists
 
-		# Return a "board is full message"
+		# Return a "board is full" message
 		return null;
+		
+# TODO: Create function for generating items if the item is a generator
+# This would involve detecting clicks/double-clicks
+# Click - select icon and show desc
+# Double-click - generate new item
+#func touch_input():
+	#if Input.is_action_just_pressed("ui_touch"):
+		#first_touch = get_global_mouse_position();
+		#var grid_position = pixel_to_grid(first_touch.x, first_touch.y);
+		#if is_in_grid(grid_position.x, grid_position.y):
+			#controlling = true;
+		#print(grid_position);
+	#if Input.is_action_just_released("ui_touch"):
+		#final_touch = get_global_mouse_position();
+		#var grid_position = pixel_to_grid(final_touch.x, final_touch.y);
+		#if is_in_grid(grid_position.x, grid_position.y) && controlling:
+			#touch_difference(pixel_to_grid(first_touch.x, first_touch.y), grid_position);
+			#controlling = false;
+
+func on_move_item(first_pos, final_pos):
+	var first_grid_pos = pixel_to_grid(first_pos.x, first_pos.y);
+	var final_grid_pos = pixel_to_grid(final_pos.x, final_pos.y);
+	var first_item = grid[first_grid_pos.x][first_grid_pos.y];
+	if is_in_grid(first_grid_pos.x, first_grid_pos.y) && is_in_grid(final_grid_pos.x, final_grid_pos.y):
+		process_pieces(first_grid_pos, final_grid_pos);
+	else:
+		first_item.move(grid_to_pixel(first_grid_pos.x, first_grid_pos.y));
+
+func process_pieces(first_grid_pos, final_grid_pos):
+	var first_item = grid[first_grid_pos.x][first_grid_pos.y];
+	var other_item = grid[final_grid_pos.x][final_grid_pos.y];
+	# If they are the same item, then merge it
+	if first_item && other_item:
+		if first_item.item_type == other_item.item_type && first_item.item_level == other_item.item_level:
+			first_item.queue_free();
+			other_item.update_level();
+			grid[final_grid_pos.x][final_grid_pos.y] = other_item;
+		else:
+			grid[first_grid_pos.x][first_grid_pos.y] = other_item;
+			grid[final_grid_pos.x][final_grid_pos.y] = first_item;
+			if first_item:
+				first_item.move(grid_to_pixel(final_grid_pos.x, final_grid_pos.y))
+			if other_item:
+				other_item.move(grid_to_pixel(first_grid_pos.x, first_grid_pos.y));
+	else:
+	# Else, swap tiles
+		grid[first_grid_pos.x][first_grid_pos.y] = other_item;
+		grid[final_grid_pos.x][final_grid_pos.y] = first_item;
+		if first_item:
+			first_item.move(grid_to_pixel(final_grid_pos.x, final_grid_pos.y))
+		if other_item:
+			other_item.move(grid_to_pixel(first_grid_pos.x, first_grid_pos.y));
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+#	touch_input();
+	pass;
--- Scripts/item.gd
+++ Scripts/item.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Sprite2D
 
 class_name Item
 
@@ -6,12 +6,53 @@ class_name Item
 @export var item_level: int;
 @export var item_sprite: Sprite2D;
 
-@onready var tree_sprite = $Sprite;
+var level1Img = load("res://Assets/Icons/test_item1.png");
+var level2Img = load("res://Assets/Icons/test_item2.png");
+var level3Img = load("res://Assets/Icons/test_item3.png");
 
-var defaultImg = load("res://Assets/Icons/test_item1.png");
+var first_position = Vector2(0, 0);
+var final_position = Vector2(0, 0);
 
-func _init(p_type: String = "music", p_level: int = 1, p_sprite: Sprite2D = tree_sprite):
+var is_dragging = false;
+var mouse_offset;
+var delay = 2;
+
+func _physics_process(delta: float):
+	if is_dragging == true:
+		var tween = get_tree().create_tween();
+		tween.tween_property(self, "position", get_global_mouse_position() - mouse_offset, delay * delta);
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			if get_rect().has_point(to_local(event.position)):
+				first_position = event.position;
+				is_dragging = true;
+				mouse_offset = get_global_mouse_position() - global_position;
+		else:
+			if get_rect().has_point(to_local(event.position)) && is_dragging:
+				final_position = event.position;
+				is_dragging = false;
+				global.move_item.emit(first_position, final_position);
+
+func _init(p_type: String = "music", p_level: int = 1):
 	item_type = p_type;
 	item_level = p_level;
-	item_sprite = p_sprite;
-	#textureImage = p_texture;
+
+func _ready():
+	image_manager(item_level);
+
+func move(target):
+	var move_tween = create_tween();
+	move_tween.set_trans(Tween.TRANS_LINEAR);
+	move_tween.set_ease(Tween.EASE_OUT);
+	move_tween.tween_property(self, "position", target, 0.1);
+	move_tween.play();
+
+func update_level():
+	item_level = item_level + 1;
+	image_manager(item_level);
+	
+func image_manager(level):
+	var texture_image = load("res://Assets/Icons/test_item" + str(level) + ".png");
+	self.texture = texture_image;
